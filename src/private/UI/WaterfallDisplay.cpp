a75449cfc95aa267d8d993ab669c25f740adc3cd
Marcel Schulz <83099491+blackcan1122@users.noreply.github.com>
2025-04-25 18:16:50 +0200

    Waterfall Refinement (#15)

--- src/private/UI/WaterfallDisplay.cpp
+++ src/private/UI/WaterfallDisplay.cpp
@@ -4,6 +4,7 @@
 #include "raylib.h"
 #include <chrono>
 #include <string>
+#include "omp.h"
 
 Waterfall::Waterfall(int Width, int Height, int TimeFrame)
     : Display(Width, Height), WorkerDone(true), RenderReady(false), TimeFrameInSec(TimeFrame)
@@ -36,6 +37,7 @@ Waterfall::~Waterfall()
 
 void Waterfall::Tick(float DeltaTime) 
 {
+    Timer<std::chrono::microseconds> t("Waterfall Tick");
     auto CurrentWorld = GetOutter()->GetWorld().TryLoad();
     m_CurrentAmbientLevel.clear();
     if (CurrentWorld)
@@ -43,6 +45,8 @@ void Waterfall::Tick(float DeltaTime)
         m_CurrentAmbientLevel = CurrentWorld->GetAmbientLevel();
     }
 
+    // change to always accumulate data and process them, no matter how many lines we shift
+    // but reset the sampled data when we shift
 
     // Temp to change index of signal
     if (IsKeyDown(KEY_A))
@@ -54,20 +58,26 @@ void Waterfall::Tick(float DeltaTime)
         Index++;
     }
 
-    AccDelta += DeltaTime;
+    if (m_AccumulatedSignals.empty())
+    {
+        m_AccumulatedSignals.resize(m_CurrentAmbientLevel.size(), 0);
+    }
 
+    #pragma omp parallel for
+    for (int i = 0; i < m_CurrentAmbientLevel.size(); ++i)
+    {
+        m_AccumulatedSignals[i] += m_CurrentAmbientLevel[i];
+    }
+
+    m_AccumulatedSignals[Index] += 255;
+    ++Counter;
+
+    AccDelta += DeltaTime;
 
     // Calculate lines to process
     float TimeStep = TimestepPerPixel();
 
-    int LinesToShift = 0;
-    {
-        LinesToShift = static_cast<int>(AccDelta / TimeStep);
-        if (LinesToShift > 0)
-        {
-            AccDelta -= LinesToShift * TimeStep;
-        }
-    }
+    int LinesToShift = static_cast<int>(AccDelta / TimeStep);
 
     if (WorkerDone.load() && RenderReady.load())
     {
@@ -75,40 +85,52 @@ void Waterfall::Tick(float DeltaTime)
         UpdateTexture(FrontTexture, FrontBuffer->PixelArray.data());
         std::swap(FrontBuffer, BackBuffer);
         RenderReady.store(false);
+
+
     }
 
     if (LinesToShift > 0 && WorkerDone.load())
     {
         WorkerDone.store(false);
-        WorkerFuture = std::async(std::launch::async, [this, LinesToShift]()
+        int temp = Counter;
+        WorkerFuture = std::async(std::launch::async, [this, LinesToShift, temp]()
             {
-                ProcessBackBuffer(LinesToShift);
+                ProcessBackBuffer(LinesToShift, std::move(m_AccumulatedSignals), temp);
                 WorkerDone.store(true);
                 RenderReady.store(true);
+
             });
+
+        Counter = 0;
+        AccDelta -= LinesToShift * TimeStep;
+
     }
 
     Draw();
     RenderToMainBuffer();
 }
 
-void Waterfall::ProcessBackBuffer(int LinesToShift) 
+void Waterfall::ProcessBackBuffer(int LinesToShift, std::vector<int> AccumulatedSamples, int AmountOfSamples) 
 {
+    std::vector<int> AverageSignals;
+    if (AccumulatedSamples.size() > 0)
+    {
+        AverageSignals.resize(AccumulatedSamples.size());
+        for (size_t i = 0; i < AccumulatedSamples.size(); ++i)
+        {
+            AverageSignals[i] = static_cast<int>(AccumulatedSamples[i] / (AmountOfSamples + 1));
+        }
+    }
+
     for (int line = 0; line < LinesToShift; line++)
     {
         BackBuffer->ShiftPixelDown();
-        for (int i = 0; i < BackBuffer->m_Width; i++)
+        for (size_t x = 0; x < AverageSignals.size(); ++x)
         {
-            if (i == Index)
-            {
-                // just some quick hack to make signal line wider
-                PixelData myPixel(0, 80, 0, 255);
-                (*BackBuffer)[i] = myPixel;
-                continue;
-            }
+            int intensity = AverageSignals.empty() ? AccumulatedSamples[x] : AverageSignals[x];
 
-            PixelData myPixel(0, m_CurrentAmbientLevel[i], 0, 255);
-            (*BackBuffer)[i] = myPixel;
+                PixelData p(0, intensity, 0, 255);
+                (*BackBuffer)[x] = p;
         }
     }
 }
